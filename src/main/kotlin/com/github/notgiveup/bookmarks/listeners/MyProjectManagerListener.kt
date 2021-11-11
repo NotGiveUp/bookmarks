1b42746eb2ac73f1ffc94aa85694ea4eaeac1c01
GitHub Action <action@github.com>
2021-11-11 03:02:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/notgiveup/bookmarks/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/notgiveup/bookmarks/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.notgiveup.bookmarks.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.notgiveup.bookmarks.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/notgiveup/bookmarks/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/notgiveup/bookmarks/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.notgiveup.bookmarks.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.notgiveup.bookmarks.MyBundle
 
 class MyApplicationService {
 
